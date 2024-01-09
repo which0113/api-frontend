70d0682564783e63a1352c8f1c988052271828ae
which <whichbei@gmail.com>
2024-01-09 22:51:05 +0800

    add 错误码

--- src/enum/ErrorCodeEnum.ts
+++ src/enum/ErrorCodeEnum.ts
@@ -5,11 +5,11 @@ enum ErrorCode {
   NOT_LOGIN_ERROR = 40100,
   NO_AUTH_ERROR = 40101,
   NOT_FOUND_ERROR = 40400,
+  TOO_MANY_REQUEST = 42900,
   FORBIDDEN_ERROR = 40300,
   SYSTEM_ERROR = 50000,
   OPERATION_ERROR = 50001,
 }
-
 
 // 错误码和错误信息对象
 const errorMessages = {
@@ -18,6 +18,7 @@ const errorMessages = {
   [ErrorCode.NOT_LOGIN_ERROR]: "未登录",
   [ErrorCode.NO_AUTH_ERROR]: "无权限",
   [ErrorCode.NOT_FOUND_ERROR]: "请求数据不存在",
+  [ErrorCode.TOO_MANY_REQUEST]: "请求过于频繁",
   [ErrorCode.FORBIDDEN_ERROR]: "禁止访问",
   [ErrorCode.SYSTEM_ERROR]: "系统内部异常",
   [ErrorCode.OPERATION_ERROR]: "操作失败",
@@ -38,6 +39,11 @@ export const errorCode = [
     code: 40101,
     name: 'NO_AUTH_ERROR',
     des: '无权限',
+  },
+  {
+    code: 42900,
+    name: 'TOO_MANY_REQUEST',
+    des: '请求过于频繁',
   },
   {
     code: 40300,
